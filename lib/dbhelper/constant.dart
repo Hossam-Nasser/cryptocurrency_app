18709de2e3b3781c83d21e9e4e072f11a50c177f
hossam nasser <hossamnasser100@gmail.com>
2022-10-13 14:51:57 +0200

    google maps

--- lib/dbhelper/constant.dart
+++
@@ -1,4 +0,0 @@
-final String tableNote = 'note';
-final String columnId = 'id';
-final String columnTitle = 'title';
-final String columnDescription = 'description';
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:intro_flutter/map_screen.dart';
 
 
-import 'screens_todo/home_screen.dart';
+
+
 
 
 
@@ -10,17 +12,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
-      ),
-      home: HomeScreen(),
+      home: MapSample(),
     );
   }
 }
